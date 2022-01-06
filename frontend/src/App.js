46495aa345e197d01522b6c5720daf998c237034
heidichiu <heidi@xqtem.com>
2022-01-07 06:19:18 +0800

    Connect frontend with backend URL and configure CrossOrigin policy

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,8 @@
+import axios from "axios";
+import baseUrl from "./config";
+
 const App = () => {
+  axios(`${baseUrl}/api/v1/books`).then((books) => console.log(books));
   return <div>First App component</div>;
 };
 
