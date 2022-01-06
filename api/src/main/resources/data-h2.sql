70e9d679206d40b24b9805977b4164e46fea8bbc
heidichiu <heidi@xqtem.com>
2022-01-07 05:43:08 +0800

    Delete rows in the table before initialization

--- api/src/main/resources/data-h2.sql
+++ api/src/main/resources/data-h2.sql
@@ -1,1 +1,2 @@
+DELETE FROM book
 insert into book(id, title, description, release_year) values ('02a363f2-4393-4d5d-afbd-54042317e478', 'Harry Potter I','Harry Potter has never even heard of Hogwarts when the letters start dropping on the doormat at number four, Privet Drive.', 1997)
