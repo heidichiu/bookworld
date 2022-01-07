7e9cf917eb84329a2bbfb9fccb77bf2f7934cd78
heidichiu <heidi@xqtem.com>
2022-01-08 02:13:45 +0800

    Add BookList test and BookListItem test

---
+++ frontend/src/components/book/__tests__/BookList.test.jsx
@@ -0,0 +1,32 @@
+import { render } from "@testing-library/react";
+import BookList from "../BookList";
+import BookListItem from "../BookListItem";
+
+jest.mock("../BookListItem");
+describe("BookList", () => {
+  beforeAll(() => {
+    BookListItem.mockImplementation(() => <div>Mock booklist item</div>);
+  });
+  const books = [
+    {
+      id: 1,
+      title: "test title",
+      description: "desc",
+      releaseYear: 2020,
+    },
+    {
+      id: 2,
+      title: "test title 2",
+      description: "desc 2",
+      releaseYear: 2022,
+    },
+  ];
+
+  it("render booklist without error", () => {
+    render(<BookList books={books} />);
+
+    expect(BookListItem).toHaveBeenCalledTimes(2);
+    expect(BookListItem).toHaveBeenCalledWith({ book: books[0] }, {});
+    expect(BookListItem).toHaveBeenCalledWith({ book: books[1] }, {});
+  });
+});
---
+++ frontend/src/components/book/__tests__/BookListItem.test.jsx
@@ -0,0 +1,17 @@
+import { render, screen } from "@testing-library/react";
+import BookListItem from "../BookListItem";
+
+describe("BookListItem", () => {
+  it("should render BookListItem without error", () => {
+    const book = {
+      id: "1",
+      title: "test title",
+      description: "desc",
+      releaseYear: 2020,
+    };
+    render(<BookListItem book={book} />);
+    expect(screen.getAllByText("test title")[0]).toBeInTheDocument();
+    expect(screen.getByText("desc")).toBeInTheDocument();
+    expect(screen.getByText("2020")).toBeInTheDocument();
+  });
+});
