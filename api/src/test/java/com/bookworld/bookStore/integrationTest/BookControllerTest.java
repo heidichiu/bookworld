3cfcc9e71d2d7797d12d8a6598fd6be73744ed3a
heidichiu <heidi@xqtem.com>
2022-01-08 13:37:03 +0800

    Add JWT authentication to the backend

--- api/src/test/java/com/bookworld/bookStore/integrationTest/BookControllerTest.java
+++ api/src/test/java/com/bookworld/bookStore/integrationTest/BookControllerTest.java
@@ -1,14 +1,20 @@
 package com.bookworld.bookStore.integrationTest;
 
 import com.bookworld.bookStore.BookStoreApplication;
+import com.bookworld.bookStore.config.JwtUtil;
 import com.bookworld.bookStore.dto.BookDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.jdbc.Sql;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -23,9 +29,30 @@ public class BookControllerTest {
     @Autowired
     private TestRestTemplate testRestTemplate;
 
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    void setUpHeader() {
+        String token = jwtUtil.generateToken(new User("peter@gmail.com", passwordEncoder.encode("password"),
+                new ArrayList<>()));
+
+        testRestTemplate.getRestTemplate().setInterceptors(
+                Collections.singletonList(((request, body, execution) -> {
+                    request.getHeaders()
+                            .add("Authorization", "Bearer " + token);
+
+                    return execution.execute(request, body);
+                }))
+        );
+    }
+
     @Test
     @Sql(scripts = {"classpath:InsertInitialBookRecordsForTest.sql"})
     void shouldReturnBooksWhenBookApiCalled() {
+        setUpHeader();
         BookDto[] listOfBooks = testRestTemplate.getForObject("http://localhost:" + port + "/api/v1/books", BookDto[].class);
 
 
@@ -36,6 +63,7 @@ public class BookControllerTest {
     @Test
     @Sql(scripts = {"classpath:InsertInitialBookRecordsForTest.sql"})
     void shouldReturnOneBookWhenCalledWithTestTitle() {
+        setUpHeader();
         BookDto[] booksDto = testRestTemplate.getForObject("http://localhost:" + port + "/api/v1/books/test Title", BookDto[].class);
         assertThat(booksDto).isNotNull();
         assertThat(booksDto.length).isEqualTo(1);
