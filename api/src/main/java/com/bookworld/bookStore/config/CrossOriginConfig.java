0c319247e4b01128efdd3441136c8da40a0db219
LAPTOP-8JRVIBUR\Heidi <heidichiu1025@gmail.com>
2022-01-09 15:16:29 +0000

    Edit cors configuration

--- api/src/main/java/com/bookworld/bookStore/config/CrossOriginConfig.java
+++ api/src/main/java/com/bookworld/bookStore/config/CrossOriginConfig.java
@@ -9,6 +9,9 @@ public class CrossOriginConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry
+                .addMapping("/**")
+                .allowedOrigins("*");
+        ;
     }
 }
