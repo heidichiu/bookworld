adaafe471ebec0cb1adf5cc3b2c920303a3ac80b
LAPTOP-8JRVIBUR\Heidi <heidichiu1025@gmail.com>
2022-01-09 19:56:53 +0000

    Add BookReviewService test

--- api/src/main/java/com/bookworld/bookStore/service/BookReviewService.java
+++ api/src/main/java/com/bookworld/bookStore/service/BookReviewService.java
@@ -14,6 +14,7 @@ import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
+
 @Service
 public class BookReviewService {
     private final BookReviewRepository bookReviewRepository;
--- api/src/test/java/com/bookworld/bookStore/service/BookReviewServiceTest.java
+++ api/src/test/java/com/bookworld/bookStore/service/BookReviewServiceTest.java
@@ -1,30 +1,55 @@
 package com.bookworld.bookStore.service;
 
+import com.bookworld.bookStore.dto.BookReviewDto;
+import com.bookworld.bookStore.model.BookReview;
+import com.bookworld.bookStore.model.User;
 import com.bookworld.bookStore.repository.BookRepository;
 import com.bookworld.bookStore.repository.BookReviewRepository;
 import com.bookworld.bookStore.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
+
+import java.util.UUID;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class BookReviewServiceTest {
     @InjectMocks
     BookReviewService bookReviewService;
     @Mock
-   BookReviewRepository bookReviewRepository;
+    BookReviewRepository bookReviewRepository;
     @Mock
-     BookRepository bookRepository;
+    BookRepository bookRepository;
+
     @Mock
     ModelMapper modelMapper;
     @Mock
     UserRepository userRepository;
 
     @Test
-    void addBookReviewToBookByUser_success(){
+    void whenAddBookReviewToBookByUser_callRepositoryToSave() {
+        UUID uuid = UUID.randomUUID();
+        UUID bookId = UUID.randomUUID();
+        when(userRepository.findUserByEmail(anyString())).thenReturn(new User(uuid, "name", "email", "password", null));
 
+        BookReviewDto reviewDto = BookReviewDto.builder().build();
+
+        bookReviewService.addBookReviewToBookByUser(reviewDto, bookId, "userEmail");
+
+
+        reviewDto.setUserId(uuid);
+        reviewDto.setBookId(bookId);
+        ArgumentCaptor<BookReview> argument = ArgumentCaptor.forClass(BookReview.class);
+        verify(bookReviewRepository).saveAndFlush(argument.capture());
+        assertThat(reviewDto).isEqualTo(reviewDto);
     }
 }
