a9ccb9a9fbb75f2bbd1a1c28ee1246c0ad9191f0
heidichiu <heidi@xqtem.com>
2022-01-10 06:09:39 +0800

    Update URI

--- frontend/src/module/book/bookService.js
+++ frontend/src/module/book/bookService.js
@@ -2,4 +2,4 @@ import axios from "axios";
 import baseurl from "../../config";
 
 export const getBooksService = () => axios.get(`${baseurl}/api/v1/books`);
-export const getBooksByTitleService = (title) => axios.get(`${baseurl}/api/v1/books/${title}`);
+export const getBooksByTitleService = (title) => axios.get(`${baseurl}/api/v1/books?title=${title}`);
