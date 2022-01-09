c89f00b65a529858027a914f057f19eb6873912d
LAPTOP-8JRVIBUR\Heidi <heidichiu1025@gmail.com>
2022-01-09 15:57:01 +0000

    Change table name to users

--- api/src/main/java/com/bookworld/bookStore/model/User.java
+++ api/src/main/java/com/bookworld/bookStore/model/User.java
@@ -5,10 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.UUID;
 
 @Data
@@ -16,6 +13,7 @@ import java.util.UUID;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO, generator = "UUID")
