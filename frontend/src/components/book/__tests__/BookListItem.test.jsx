853a08b19488cfd06b193180e931c2647798e477
heidichiu <heidi@xqtem.com>
2022-01-10 15:33:05 +0800

    Update snapshot and fix BookListItem test

--- frontend/src/components/book/__tests__/BookListItem.test.jsx
+++ frontend/src/components/book/__tests__/BookListItem.test.jsx
@@ -10,7 +10,7 @@ describe("BookListItem", () => {
       description: "desc",
       releaseYear: 2020,
     };
-    renderWithRedux(<BookListItem book={book} />);
+    renderWithRedux(<BookListItem book={book} />, {});
     expect(screen.getAllByText("test title")[0]).toBeInTheDocument();
     expect(screen.getByText("desc")).toBeInTheDocument();
     expect(screen.getByText("2020")).toBeInTheDocument();
